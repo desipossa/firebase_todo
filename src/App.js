59f5ce80cdf4d0ece32815226d1b1506839ad15f
lee <sipossa@naver.com>
2022-10-18 14:20:33 +0900

    20221018

--- src/App.js
+++ src/App.js
@@ -1,41 +1,79 @@
 import { async } from '@firebase/util';
-import { collection, getDocs, onSnapshot } from 'firebase/firestore';
+import { collection, getDocs, onSnapshot, addDoc, updateDoc, serverTimestamp, deleteDoc, doc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { db } from './firebase';
 
 
 
 const App = () => {
-    const [todos, setTodos] = useState([]);
+    const [list, setList] = useState([]);
+    const [input, setInput] = useState({});
+
+
+    //firebase db에서 읽어오기
+
     const getdb = async () => {
-        // const querySnapshot = await getDocs(collection(db, "todo"));
-        // setTodos(querySnapshot.doc());
         onSnapshot(collection(db, 'todo'), (snapshot) => {
-            setTodos(snapshot.docs.map(doc => doc.data()))
+            setList(snapshot.docs.map(doc => ({
+                id: doc.id,
+                item: doc.data()
+            })))
         })
     }
     useEffect(() => {
         getdb()
     }, [])
-    // db.collection('todo')
-    //     .orderBy('todo')
-    //     .onSnapshot(data => {
-    //         setTodos(data)
-    //     })
+
+
+    //firebase db에 추가
+
+    const inputHandler = e => {
+        const { name, value } = e.target;
+        setInput({
+            ...input,
+            [name]: value
+        })
+    }
+
+    const addHandler = (e) => {
+        e.preventDefault();
+        addDoc(collection(db, 'todo'), {
+            content: input,
+            timestamp: serverTimestamp()
+        })
+    };
+
+    const deleteHandler = (id) => {
+        deleteDoc(doc(db, 'todo', id))
+    }
+
+    const updateHandler = (id) => {
+        updateDoc(doc(db, 'todo', id), { content: input });
+    }
     return (
         <div>
             <h1>짜잔...</h1>
+            <form onSubmit={addHandler}>
+                <input name='name' value={input.value} onChange={inputHandler} />
+                <input name='title' value={input.value} onChange={inputHandler} />
+                <textarea name='content' value={input.value} onChange={inputHandler} />
+                <button>추가</button>
+            </form>
+
             <ul>
                 {
-                    todos.map(it => <li>{it.title}</li>)
+                    list.map(it => <li>
+                        {it.item.content.name}
+                        {it.item.content.title}
+                        {it.item.content.content}
+                        {/* {Date(it.item.timestamp.seconds)} */}
+                        <button onClick={() => deleteHandler(it.id)}>X</button>
+                        <button onClick={() => updateHandler(it.id)}>m</button>
+                    </li>
+                    )
                 }
             </ul>
-            {/* {
-                querySnapshot.forEach((doc) => {
-                    console.log(doc);
-                })
-            } */}
-            {console.log(todos, db)}
+            {console.log(list, db)}
         </div>
     )
 }
